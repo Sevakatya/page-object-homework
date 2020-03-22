e87eef3e25f3003de4c0c310c4f546630b0ded05
Sevakatya <sevakatya@gmail.com>
2020-03-22 13:27:19 +0200

    Third commit, module 4, homework

---
+++ test_main_page.py
@@ -0,0 +1,18 @@
+from pages.main_page import MainPage
+from pages.base_page import BasePage
+import pytest
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_can_go_to_login_page(browser):
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)
+    # открываем страницу
+    page.open()
+    # выполняем метод страницы - переходим на страницу логина
+    page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()

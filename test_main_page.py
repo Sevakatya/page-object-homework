45cb27b1bdabe1898c21c6b935e3630d7c66f24e
Sevakatya <sevakatya@gmail.com>
2020-03-22 16:57:35 +0200

    Added transition between pages by returning the desired page object

--- test_main_page.py
+++ test_main_page.py
@@ -2,15 +2,15 @@ from pages.main_page import MainPage
 from pages.base_page import BasePage
 import pytest
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+link = "http://selenium1py.pythonanywhere.com"
 
 def test_guest_can_go_to_login_page(browser):
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page = MainPage(browser, link)
-    # открываем страницу
     page.open()
-    # выполняем метод страницы - переходим на страницу логина
     page.go_to_login_page()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
